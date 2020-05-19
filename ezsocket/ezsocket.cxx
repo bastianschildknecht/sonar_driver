ccac404b8abb5b76629367c98ce9389d3fffd38d
Bastian Schildknecht <sbastian@student.ethz.ch>
2020-05-19 14:59:32 +0200

    Add basic libezsocket implementation for linux

--- ezsocket/ezsocket.cxx
+++ ezsocket/ezsocket.cxx
@@ -1,9 +1,184 @@
+#include <ezsocket.hxx>
+
 #ifdef _WIN32
 
-#elif __linux__
-
-#elif __APPLE__
-
 #else
-#error "OS not supported!"
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
 #endif
+
+using namespace EZSocket;
+
+Socket::Socket()
+{
+}
+
+Socket::~Socket()
+{
+}
+
+void Socket::connectToHost(const char *hostname, uint16_t port)
+{
+    if (state == SocketState::Ready)
+    {
+#ifdef _WIN32
+#else
+        host_addr.sin_family = AF_INET;
+        host_addr.sin_port = htons(port);
+
+        if (inet_pton(AF_INET, hostname, &host_addr.sin_addr) <= 0)
+        {
+            state = SocketState::ConnectError;
+            return;
+        }
+
+        if (connect(socket_fd, (struct sockaddr *)&host_addr, sizeof(host_addr)) < 0)
+        {
+            state = SocketState::ConnectError;
+            return;
+        }
+#endif
+        state = SocketState::Connected;
+    }
+}
+
+void Socket::disconnect()
+{
+    if (state == SocketState::Connected)
+    {
+#ifdef _WIN32
+#else
+        close(socket_fd);
+        initSocket();
+#endif
+    }
+}
+
+int32_t Socket::readData(void *buffer, int32_t maxLength)
+{
+    if (state == SocketState::Connected)
+    {
+#ifdef _WIN32
+        return 0;
+#else
+        return read(socket_fd, buffer, (size_t)maxLength);
+#endif
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int32_t Socket::writeData(const void *buffer, int32_t length)
+{
+    if (state == SocketState::Connected)
+    {
+#ifdef _WIN32
+        return 0;
+#else
+        return write(socket_fd, buffer, (size_t)length);
+#endif
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+void Socket::setReceiveBufferSize(int32_t size)
+{
+    if (state == SocketState::Ready)
+    {
+        size = size >= 1024 ? size : 1024;
+#ifdef _WIN32
+#else
+        setsockopt(socket_fd, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
+#endif
+    }
+}
+
+uint64_t Socket::bytesAvailable()
+{
+    int32_t value = 0;
+    if (state == SocketState::Connected)
+    {
+#ifdef _WIN32
+#else
+        ioctl(socket_fd, FIONREAD, &value);
+#endif
+    }
+    return value;
+}
+
+SocketState Socket::getState()
+{
+    return state;
+}
+
+TCPSocket::TCPSocket() : Socket()
+{
+    TCPSocket::initSocket();
+}
+
+TCPSocket::~TCPSocket()
+{
+#ifdef _WIN32
+#else
+    if (state != SocketState::InitError)
+    {
+        close(socket_fd);
+    }
+#endif
+}
+
+void TCPSocket::initSocket()
+{
+#ifdef _WIN32
+#else
+    if (socket_fd = socket(AF_INET, SOCK_STREAM, 0) < 0)
+    {
+        state = SocketState::InitError;
+    }
+    else
+    {
+        state = SocketState::Ready;
+    }
+#endif
+}
+
+UDPSocket::UDPSocket() : Socket()
+{
+    UDPSocket::initSocket();
+}
+
+UDPSocket::~UDPSocket()
+{
+#ifdef _WIN32
+#else
+    if (state != SocketState::InitError)
+    {
+        close(socket_fd);
+    }
+#endif
+}
+
+void UDPSocket::initSocket()
+{
+#ifdef _WIN32
+#else
+    if (socket_fd = socket(AF_INET, SOCK_DGRAM, 0) < 0)
+    {
+        state = SocketState::InitError;
+    }
+    else
+    {
+        state = SocketState::Ready;
+    }
+#endif
+}
--- ezsocket/ezsocket.hxx
+++ ezsocket/ezsocket.hxx
@@ -1,14 +1,87 @@
+// File: ezsocket.hxx
+
 #ifndef EZSOCKET_HXX
 #define EZSOCKET_HXX
 
+#include <cstdint>
+
 #ifdef _WIN32
-
-#elif __linux__
-
-#elif __APPLE__
-
 #else
-#error "OS not supported!"
+
+#include <netinet/in.h>
+
 #endif
+
+namespace EZSocket
+{
+
+    enum SocketState
+    {
+        Ready,
+        Connected,
+        InitError,
+        ConnectError
+    };
+
+    class Socket
+    {
+    public:
+        Socket();
+        virtual ~Socket() = 0;
+
+        // Connects the socket to the specified host on the specified port
+        // (blocking)
+        virtual void connectToHost(const char *hostname, uint16_t port);
+
+        // Disconnects the socket from the host
+        virtual void disconnect();
+
+        // Reads available bytes from the socket into a buffer up to a maximum length
+        virtual int32_t readData(void *buffer, int32_t maxLength);
+
+        // Writes a specified amount of bytes to the socket
+        virtual int32_t writeData(const void *buffer, int32_t length);
+
+        // Changes the size of the socket receive buffer
+        // (Must be used before establishing a connection)
+        virtual void setReceiveBufferSize(int32_t size);
+
+        // Returns the number of received bytes on the socket
+        virtual uint64_t bytesAvailable();
+
+        // Get the current state the socket is in (includes errors)
+        virtual SocketState getState();
+
+    protected:
+        int socket_fd;
+        SocketState state;
+#ifdef _WIN32
+#else
+        struct sockaddr_in host_addr;
+#endif
+        virtual void initSocket() = 0;
+    };
+
+    class TCPSocket : public Socket
+    {
+    public:
+        TCPSocket();
+        ~TCPSocket();
+
+    protected:
+        void initSocket();
+    };
+
+    class UDPSocket : public Socket
+    {
+    public:
+        UDPSocket();
+        ~UDPSocket();
+
+    protected:
+        void initSocket();
+    };
+
+} // namespace EZSocket
 
 #endif /* EZSOCKET_HXX */
