11bcce82de33f26ab9e512b0de403d5a09037cf8
Bastian Schildknecht <sbastian@student.ethz.ch>
2020-06-01 22:39:19 +0200

    Add partial implementation of the sonardevices lib

---
+++ sonardevices/sonardevices.cxx
@@ -0,0 +1,117 @@
+// File: sonardevices.cxx
+
+#include <iostream>
+#include <ezsocket.hxx>
+#include <sonardevices.hxx>
+
+using namespace EZSocket;
+using namespace SonarDevices;
+
+Sonar::Sonar()
+{
+    state = SonarState::Ready;
+}
+
+Sonar::~Sonar()
+{
+}
+
+void Sonar::registerCallback(void (*callback)(SonarImage *sonarImage))
+{
+}
+
+SonarState Sonar::getState()
+{
+    return state;
+}
+
+OculusSonar::OculusSonar() : Sonar()
+{
+    sonarTCPSocket = new TCPSocket();
+    sonarUDPSocket = new UDPSocket();
+    sonarAddress = new char[STR_ADDRESS_LEN];
+}
+
+OculusSonar::~OculusSonar()
+{
+    delete sonarTCPSocket;
+    sonarTCPSocket = nullptr;
+    delete sonarUDPSocket;
+    sonarUDPSocket = nullptr;
+    delete[] sonarAddress;
+    sonarAddress = nullptr;
+}
+
+void OculusSonar::findAndConnect()
+{
+    if (sonarUDPSocket->getState() == SocketState::Ready)
+    {
+        sonarUDPSocket->bindToAddress("ANY", OCULUS_UDP_PORT);
+        if (sonarUDPSocket->getState() == SocketState::Bound)
+        {
+            char *buf = new char[256];
+            if (sonarUDPSocket->waitForDataAndAddress(buf, 256, sonarAddress, STR_ADDRESS_LEN) > 0) {
+                connect(sonarAddress);
+            }
+            delete[] buf;
+            buf = nullptr;
+        }
+    }
+}
+
+void OculusSonar::connect(const char *address)
+{
+    if (sonarTCPSocket->getState() == SocketState::Ready)
+    {
+        sonarTCPSocket->connectToHost(address, OCULUS_TCP_PORT);
+        if (sonarTCPSocket->getState() == SocketState::Connected)
+        {
+            strncpy(sonarAddress, address, STR_ADDRESS_LEN);
+            state = SonarState::Connected;
+        }
+    }
+}
+
+void OculusSonar::disconnect()
+{
+    sonarUDPSocket->disconnect();
+    sonarTCPSocket->disconnect();
+    if (sonarUDPSocket->getState() == SocketState::Ready && sonarTCPSocket->getState() == SocketState::Ready)
+    {
+         state = SonarState::Ready;
+    }
+    else
+    {
+        state == SonarState::SocketError;
+    }
+}
+
+void OculusSonar::fire()
+{
+}
+
+const char *OculusSonar::getLocation()
+{
+    return sonarAddress;
+}
+
+SonarImage *OculusSonar::getLastImage()
+{
+    return NULL;
+}
+
+OculusM1200d::OculusM1200d() : OculusSonar()
+{
+}
+
+OculusM1200d::~OculusM1200d()
+{
+}
+
+SonarImage::SonarImage()
+{
+}
+
+SonarImage::~SonarImage()
+{
+}
--- sonardevices/sonardevices.hxx
+++ sonardevices/sonardevices.hxx
@@ -4,39 +4,19 @@
 #define SONARDEVICES_HXX
 
 #include <cstdint>
+#include <ezsocket.hxx>
 
 namespace SonarDevices
 {
-    class Sonar
+    /**
+     * Generic definitions
+     */
+
+    enum SonarState
     {
-    public:
-        Sonar();
-        virtual ~Sonar() = 0;
-
-        // Find sonar via UDP broadcast signal and connect to any sonar (blocking)
-        virtual void findAndConnect();
-
-        // Connect to sonar at specified IPv4 address (blocking)
-        virtual void connect(const char *address);
-
-        // Disconnect from sonar (blocking)
-        virtual void disconnect();
-
-        // Register a callback function to be called, when an image is ready (non-blocking)
-        virtual void registerCallback(void (*callback)(SonarImage &sonarImage));
-
-        // Signal the sonar to aquire a new image (non-blocking)
-        virtual void fire();
-
-        // Retrieve the last sonar image captured
-        virtual SonarImage getLastImage();
-    };
-
-    class OculusM1200d : Sonar
-    {
-    public:
-        OculusM1200d();
-        ~OculusM1200d();
+        Ready,
+        Connected,
+        SocketError
     };
 
     class SonarImage
@@ -48,6 +28,80 @@ namespace SonarDevices
         uint16_t imageHeight;
         uint8_t *data[];
     };
+
+    class Sonar
+    {
+    public:
+        Sonar();
+        virtual ~Sonar() = 0;
+
+        // Find sonar via UDP broadcast signal and connect to any sonar (blocking)
+        virtual void findAndConnect() = 0;
+
+        // Connect to sonar at specified IPv4 address (blocking)
+        virtual void connect(const char *address) = 0;
+
+        // Disconnect from sonar (blocking)
+        virtual void disconnect() = 0;
+
+        // Register a callback function to be called, when an image is ready (non-blocking)
+        virtual void registerCallback(void (*callback)(SonarImage *sonarImage));
+
+        // Signal the sonar to aquire a new image (non-blocking)
+        virtual void fire() = 0;
+
+        // Retrieve the last sonar image captured
+        // If no image is available NULL is returned
+        virtual SonarImage *getLastImage() = 0;
+
+        // Get the current connection state of the sonar
+        virtual SonarState getState();
+
+        // Get a string describing the current address or interface location of the sonar
+        virtual const char *getLocation() = 0;
+
+    protected:
+        SonarState state;
+    };
+
+    /**
+     * Blueprint subsea Oculus specific definitions
+     */
+
+    class OculusSonar : public Sonar
+    {
+    public:
+        OculusSonar();
+        ~OculusSonar() = 0;
+        virtual void findAndConnect();
+        virtual void connect(const char *address);
+        virtual void disconnect();
+        virtual void fire();
+        virtual SonarImage *getLastImage();
+        virtual const char *getLocation();
+
+    protected:
+        EZSocket::Socket *sonarUDPSocket;
+        EZSocket::Socket *sonarTCPSocket;
+        char *sonarAddress;
+    };
+
+#define OCULUS_CHECK_ID 0x4f53
+
+#define OCULUS_UDP_PORT 52102
+#define OCULUS_TCP_PORT 52100
+
+    /**
+    * Oculus M1200d specific definitions
+    */
+
+    class OculusM1200d : public OculusSonar
+    {
+    public:
+        OculusM1200d();
+        ~OculusM1200d();
+    };
+
 } // namespace SonarDevices
 
 #endif
