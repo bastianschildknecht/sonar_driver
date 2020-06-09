b50ed48e3d45a9d703373ab2f9f7356f4efd301a
Bastian Schildknecht <sbastian@student.ethz.ch>
2020-06-09 16:53:06 +0200

    Implement sonardriver example

--- sonardriver/sonardriver.cxx
+++ sonardriver/sonardriver.cxx
@@ -1,38 +1,60 @@
 #include <iostream>
 #include <cstdlib>
-#include <ezsocket.hxx>
+#include <sonardevices.hxx>
 
 using namespace std;
-using namespace EZSocket;
+using namespace SonarDevices;
+
+void callback(SonarImage *img)
+{
+    cout << "Received Sonar Image!" << endl;
+    cout << "\tImage width: " << img->imageWidth << endl;
+    cout << "\tImage height: " << img->imageHeight << endl;
+    cout << "\tImage type: " << img->imageType << endl;
+    switch (img->imageType)
+    {
+    case SonarImageType::OculusSonarImageObject:
+    {
+        OculusSonarImage *osi = (OculusSonarImage *) img;
+        cout << "\tPing start time: " << osi->pingStartTime << endl;
+        cout << "\tSonar frequency: " << osi->sonarFrequency << endl;
+        cout << "\tTemperature: " << osi->temperature << endl;
+        cout << "\tPressure: " << osi->pressure << endl;
+        break;
+    }
+    case SonarImageType::OculusSonarImage2Object:
+    {
+        OculusSonarImage2 *osi2 = (OculusSonarImage2 *) img;
+        cout << "\tPing start time: " << osi2->pingStartTime << endl;
+        cout << "\tSonar frequency: " << osi2->sonarFrequency << endl;
+        cout << "\tTemperature: " << osi2->temperature << endl;
+        cout << "\tPressure: " << osi2->pressure << endl;
+        cout << "\tHeading: " << osi2->heading << endl;
+        cout << "\tPitch: " << osi2->pitch << endl;
+        cout << "\tRoll: " << osi2->roll << endl;
+        break;
+    }
+    default:
+        break;
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    int bufferSize = 1<<12;
-    
-    Socket *socket = new TCPSocket();
-    if (socket->getState() == SocketState::Ready)
+    Sonar *sonar = new OculusSonar();
+    sonar->registerCallback(callback);
+    sonar->configure(2, 5, 0.5, 0, 0, false, 1, 255);
+    sonar->setPingRate(40);
+    cout << "Looking for sonar..." << endl;
+    sonar->findAndConnect();
+    if (sonar->getState() == SonarState::Connected)
     {
-        socket->setReceiveBufferSize(2*bufferSize);
-        socket->connectToHost("192.168.1.10", 52100);
-        if (socket->getState() == SocketState::Connected)
-        {
-            char *buff = new char[bufferSize];
-            int bytesRead = socket->readData(buff,bufferSize);
-            cout << "Received " << bytesRead << " bytes" << endl;
-            cout << "Data: " << buff << endl;
-            delete[] buff;
-            buff = nullptr;
-        }
-        else
-        {
-            cout << "Could not connect!" << endl;
-        }
+        cout << "Found sonar at location: " << sonar->getLocation() << endl;
+        sonar->fire();
+        Sleep(30000);
     }
-    else
-    {
-        cout << "Initialization error!" << endl;
-    }
-    delete socket;
-    socket = nullptr;
+    sonar->disconnect();
+    delete sonar;
+    sonar = nullptr;
     exit(EXIT_SUCCESS);
 }
