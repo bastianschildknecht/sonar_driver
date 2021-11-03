bd98004feb6ba3adc20e8da872b3c52eb753fb9e
Bastian Daniel Schildknecht <34478237+bastianschildknecht@users.noreply.github.com>
2021-11-03 17:49:16 +0100

    Update oculusMessages.hxx with license information

--- include/sonar_driver/sonardevices/oculusMessages.hxx
+++ include/sonar_driver/sonardevices/oculusMessages.hxx
@@ -1,5 +1,29 @@
 // File: oculusMessages.hxx
 
+/******************************************************************************
+ * Part of this file is taken from Blueprint Subsea's Oculus Viewer software
+ * under the following license:
+ *
+ * *****
+ * (c) Copyright 2017 Blueprint Subsea.
+ * This file is part of Oculus Viewer
+ *
+ * Oculus Viewer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Oculus Viewer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * *****
+ *
+ *****************************************************************************/
+
 #ifndef OCULUSMESSAGES_HXX
 #define OCULUSMESSAGES_HXX
 
